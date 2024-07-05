1cd266da8e02dca2486352c2c19c34cae9ba90dc
Nattawat Vitchuwan <nattawat.v@tcrbank.com>
2024-07-05 12:55:48 +0700

    [Remove]origin

---
+++ src/useCases/transaction/foodTransaction.useCase.ts
@@ -0,0 +1,7 @@
+import prisma from "../../utility/db"
+import { InternalServerError } from 'http-errors'
+export default Object.freeze({
+    createFoodTransaction: async (params: {
+
+    }) => { }
+})
